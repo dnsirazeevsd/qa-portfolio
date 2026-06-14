93c497bdc657531aa2566551dd80e27b92390916
Danya Fedorov <dnsirazeev@inbox.ru>
2026-06-14 20:03:10 +0300

    feat: add new fixture logged_request

--- conftest.py
+++ conftest.py
@@ -3,8 +3,21 @@ import requests
 
 BASE_URL = "https://jsonplaceholder.typicode.com"
 
-@pytest.fixture
-def api_client():
+#Создание линка для запроса
+@pytest.fixture()
+def base_url():
     def _get(path):
         return requests.get(BASE_URL + path)
     return _get
+
+#Логирование запросов
+@pytest.fixture()
+def logged_request():
+    def _make_request(path):
+        url = BASE_URL + path
+        print(f"->Sending request to {url}")
+        response = requests.get(url=url)
+        print(f"<-Response status: {response.status_code}")
+        return response
+    return _make_request
+
--- tests/api/tests_albums.py
+++ tests/api/tests_albums.py
@@ -2,8 +2,11 @@ import pytest
 from jsonschema import validate
 from schemas.schema_albums import SCHEMA_ALBUMS
 
+#Тест с 5 параметрами - для проверки коллекции id альбомов
 @pytest.mark.parametrize("id_albums", [1, 2, 3, 4, 5])
-def test_albums_validate(api_client, id_albums):
-    response = api_client(f"/albums/{id_albums}")
+def test_albums_validate(logged_request, id_albums):
+    response = logged_request(f"/albums/{id_albums}")
     assert response.status_code == 200
     validate(instance=response.json(), schema=SCHEMA_ALBUMS)
+
+
