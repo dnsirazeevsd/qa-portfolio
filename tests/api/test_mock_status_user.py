8a3d7b47c330ec04b92adc28d396207bc7dc08b6
Danil <sirazeev00@inbox.ru>
2026-06-25 16:37:00 +0300

    test: add new mock tests

--- tests/api/test_mock_status_user.py
+++ tests/api/test_mock_status_user.py
@@ -1,13 +1,51 @@
+import pytest
 from src.profile_user import get_user_status
 
-def test_user(mocker):
+#Проверка status 200 - mock
+def test_user_200(mocker):
 
     mocker_response = mocker.Mock()
 
     mocker_response.json.return_value = {"status" : "OK"}
+    mocker_response.status_code = 200
 
     mocker.patch('src.profile_user.requests.get', return_value=mocker_response)
 
-    result = get_user_status(123)
+    response = get_user_status(123)
 
-    assert result["status"] == "OK"
+    assert response.status_code == 200
+    assert response.json()["status"] == "OK"
+
+#Проверка status 500 - mock
+def test_user_500(mocker):
+    mock_response = mocker.Mock()
+    mock_response.status_code = 500
+    mock_response.json.return_value = {"error": "server error"}
+
+    mocker.patch('src.profile_user.requests.get', return_value = mock_response)
+
+    response = get_user_status("TEST")
+
+    assert response.status_code == 500
+    assert response.json()["error"] == "server error"
+
+#Проверка status 404 - mock
+def test_user_404(mocker):
+    mock_response = mocker.Mock()
+    mock_response.status_code = 404
+    mock_response.json.return_value = {"error": "User not found"}
+
+    mocker.patch('src.profile_user.requests.get', return_value = mock_response)
+
+    response = get_user_status(12)
+
+    assert response.status_code == 404
+    assert response.json()["error"] == "User not found"
+
+#Проверка исключения через side-effect
+def test_user_raises(mocker):
+    
+    mocker.patch('src.profile_user.requests.get', side_effect=TimeoutError("Request timeout"))
+
+    with pytest.raises(TimeoutError, match="Request timeout"):
+        get_user_status(1)
