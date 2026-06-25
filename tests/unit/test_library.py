2298e409398f9f90c7b5feaab8d77c6913ed3bde
Danil <sirazeev00@inbox.ru>
2026-06-25 13:44:48 +0300

    refactor: update code

--- tests/unit/test_library.py
+++ tests/unit/test_library.py
@@ -8,6 +8,7 @@ import pytest
 def test_func_add_book(library, name_book):
     library.add_book(name_book)
     assert name_book in library.get_books() 
+
 #Удаление книги
 @pytest.mark.parametrize("name_book", 
     [   "Harry Potter",
@@ -16,6 +17,7 @@ def test_func_add_book(library, name_book):
 def test_remove_book(library_with_books, name_book):
     library_with_books.remove_book(name_book)
     assert name_book not in library_with_books.get_books()
+
 #Удаление несуществующей книги(Негативный тест)
 def test_remove_book_negative(library):
     with pytest.raises(ValueError, match="not found"):
@@ -23,6 +25,7 @@ def test_remove_book_negative(library):
 #Проверка кол-ва книг в библиотеки
 def test_count_books(library_with_books):
     assert library_with_books.get_count_books() == 3
+    
 #Проверка если ли книга в библиотеки
 @pytest.mark.parametrize("name_book", 
     [
